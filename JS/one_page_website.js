97b909cf16b99474c8bae755eb091e87d43a9c6b
carlhumphreycruz19 <carlhumphrey_cruz@yahoo.ca>
2025-06-26 01:57:17 -0400

    creating website with JS

--- JS/one_page_website.js
+++ JS/one_page_website.js
@@ -37,6 +37,8 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
+  captionText.innerHTML = dots[slideIndex-1].alt;{
+    
+  }
+
 </script>
